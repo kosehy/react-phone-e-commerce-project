5557797f95a264ed7b933377e6207a414248697a
kosehy <kosehy@gmail.com>
2019-12-08 12:13:02 -0800

    Update Product.js
    add propTypes to check whether data format is correct or not

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { ProductConsumer } from "../context";
+import PropTypes from "prop-types";
 
 class Product extends Component {
   render() {
@@ -47,34 +48,44 @@ class Product extends Component {
   }
 }
 
+Product.propTypes = {
+  product:PropTypes.shape({
+    id:PropTypes.number,
+    img:PropTypes.string,
+    title:PropTypes.string,
+    price:PropTypes.number,
+    inCart:PropTypes.bool
+  }).isRequired
+};
+
 const ProductWrapper = styled.div`
     .card {
-        border-color:transparent;
-        transition: all is linear;   
+      border-color:transparent;
+      transition: all is linear;   
     }
     .card-footer {
-        background: transparent;
-        border-top: transparent;
-        transition: all is linear;
+      background: transparent;
+      border-top: transparent;
+      transition: all is linear;
     }
     &:hover {
-        .card {
-        border:0.04rem solid rgba(0,0,0,0.2);
-        box-shadow;2px 2px 5px 0px rgba(0,0,0,0.2);
-        }
-        .card-footer {
-            background: rgba(247, 247, 247);
-        }
+      .card {
+      border:0.04rem solid rgba(0,0,0,0.2);
+      box-shadow;2px 2px 5px 0px rgba(0,0,0,0.2);
+      }
+      .card-footer {
+        background: rgba(247, 247, 247);
+      }
     }
     .img-container {
-        position: relative;
-        overflow: hidden;
+      position: relative;
+      overflow: hidden;
     }
     .card-img-top {
-        transition: all 1s linear;
+      transition: all 1s linear;
     }
     .img-container:hover .card-img-top {
-        transform: scale(1.2);
+      transform: scale(1.2);
     }
     .cart-btn {
       position:  absolute;
