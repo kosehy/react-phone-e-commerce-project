e33371abd1f6d7919b75a12203429a78de5f9c22
kosehy <kosehy@gmail.com>
2019-12-08 10:38:46 -0800

    Initial commit

---
+++ src/components/Cart.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Cart extends Component {
+    render() {
+        return (
+            <div >
+                <h3>hellow from car</h3>
+            </div>
+        );
+    }
+}
+
+export default Cart;
