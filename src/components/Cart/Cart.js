202f543048edee6b5384aec9ec7482f1e86f7543
kosehy <kosehy@gmail.com>
2019-12-08 16:46:55 -0800

    Add CartColumns.js Update Cart.js
    CartColumns.js - add strings related with cart info
    Cart.js - add CartColumns module

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,15 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
+import Title from "../Title";
+import CartColumns from './CartColumns'
 class Cart extends Component {
-    render() {
-        return (
-            <div >
-                <h3>hellow from car</h3>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <section>
+        <Title name="your" title="cart"/>
+        <CartColumns/>
+      </section>
+    )
+  }
 }
 
 export default Cart;
